b0ed43b8328451e3cc33073e54482e04bc40e6e0
Daniil <danilmux@gmail.com>
2024-12-20 20:14:48 +0500

    add specification and usage

--- Notification-Service/Application/Features/Notifications/SearchNotificationsQuery.cs
+++ Notification-Service/Application/Features/Notifications/SearchNotificationsQuery.cs
@@ -1,7 +1,9 @@
 ﻿using Notification_Service.Application.Infrastructure.CQS;
 using Notification_Service.Application.Infrastructure.Result;
 using Notification_Service.Core.Domain;
+using Notification_Service.Core.Domain.SharedKernel.Specification;
 using Notification_Service.Core.Domain.SharedKernel.Storage;
+using Notification_Service.Infrastructure.DataStorage.Repositories;
 
 namespace Notification_Service.Application.Features.Notifications
 {
@@ -22,7 +24,39 @@ namespace Notification_Service.Application.Features.Notifications
 
         public override async Task<Result<IReadOnlyList<Notification>>> Handle(SearchNotificationsQuery request, CancellationToken cancellationToken)
         {
-            var notifications = await _notificationRepository.ListAsync(x => x.Status == request.Status || x.ContentType == request.ContentType , cancellationToken);
+            ISpecification<Notification> searchNotificationSpecification = null;
+
+            var searchStatusSpecification = NotificationSpecification.SearchByStatus(request.Status);
+            var searchContentTypeSpecification = NotificationSpecification.SearchByContentType(request.ContentType);
+
+
+            searchNotificationSpecification = searchContentTypeSpecification.And(searchStatusSpecification);
+
+            if (request.ContentType == "sms")
+            {
+                searchNotificationSpecification = searchNotificationSpecification.Or(Specification<Notification>.Create(x => string.IsNullOrEmpty(x.Status)));
+            }
+
+            //IReadOnlyList<Notification> notifications = null;
+
+            //if (!string.IsNullOrEmpty(request.Status) && string.IsNullOrEmpty(request.ContentType))
+            //{
+            //    notifications = await _notificationRepository.ListAsync(x => x.Status == request.Status, cancellationToken);
+            //} 
+            //else if (!string.IsNullOrEmpty(request.ContentType) && string.IsNullOrEmpty(request.Status))
+            //{
+            //    notifications = await _notificationRepository.ListAsync(x => x.ContentType == request.ContentType, cancellationToken);
+            //} 
+            //else if (!string.IsNullOrEmpty(request.Status) && (!string.IsNullOrEmpty(request.ContentType)))
+            //{
+            //    notifications = await _notificationRepository.ListAsync(x => x.Status == request.Status || x.ContentType == request.ContentType, cancellationToken);
+            //}
+
+
+            var notifications = await _notificationRepository.ListAsync(searchNotificationSpecification, cancellationToken);
+
+            var notificationsId = await _notificationRepository.QueryAsync(x => x.Where(searchNotificationSpecification).OrderByDescending(y => y.ContentType).Select(y => y.Id), cancellationToken);
+
             return Success(notifications);
         }
     } 
--- Notification-Service/Infrastructure/DataStorage/Repositories/NotificationRepository.cs
+++ Notification-Service/Infrastructure/DataStorage/Repositories/NotificationRepository.cs
@@ -1,20 +1,26 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System.Net.Mime;
+using Microsoft.EntityFrameworkCore;
 using Notification_Service.Core.Domain;
 using Notification_Service.Core.Domain.Repositories;
+using Notification_Service.Core.Domain.SharedKernel.Specification;
 
 namespace Notification_Service.Infrastructure.DataStorage.Repositories
 {
-    public class NotificationRepository : EFRepository<Notification, ServerDbContext>, INotificationRepository
+    public sealed class NotificationRepository(ServerDbContext context) : EFRepository<Notification, ServerDbContext>(context), INotificationRepository
     {
-       private readonly ServerDbContext _context;
-        public NotificationRepository(ServerDbContext context) : base(context) 
-        {
-            _context = context;
-        }
-
         public async Task<IReadOnlyList<Notification>> SearchNotificationByStatusAndContentType(string status, string contentType, CancellationToken cancellationToken)
         {
-            return await Items.Where(x => EF.Functions.Like(x.Status, $"%{status}%") && x.ContentType == contentType).ToListAsync(cancellationToken);
+            return await this.ListAsync(NotificationSpecification.SearchByStatusAndContentType(status, contentType), cancellationToken);
         } 
     }
+
+    public static class NotificationSpecification
+    {
+        public static ISpecification<Notification> SearchByStatus(string status) => Specification<Notification>.Create(x => x.Status == status);
+        public static ISpecification<Notification> SearchByContentType(string contentType) => Specification<Notification>.Create(x => x.ContentType == contentType);
+
+        internal static ISpecification<Notification> SearchByStatusAndContentType(string status, string contentType) => SearchByStatus(status)
+            .And(Specification<Notification>.Create(x => EF.Functions.Like(nameof(x.Status), $"%{status}%")));
+    }
 }
+
