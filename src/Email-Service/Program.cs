bbbfb5d6497ae2ef915ddae93a41720b1b301801
Daniil <danilmux@gmail.com>
2025-01-03 06:08:04 +0500

    fix-edit docker-compose, - comments

--- src/Email-Service/Program.cs
+++ src/Email-Service/Program.cs
@@ -8,11 +8,14 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 // builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
-var client = new SmtpClient(builder.Configuration.GetConnectionString("smtpServer"), 
+var client = new SmtpClient(
+    builder.Configuration.GetConnectionString("smtpServer"), 
     Convert.ToInt32(builder.Configuration.GetConnectionString("smtpPort")))
 {
-    Credentials = new NetworkCredential(builder.Configuration.GetConnectionString("username"), 
-        builder.Configuration.GetConnectionString("password")),
+    Credentials = new NetworkCredential(
+        builder.Configuration.GetConnectionString("username"), 
+        builder.Configuration.GetConnectionString("password")
+    ),
     EnableSsl = true
 };
 
@@ -31,33 +34,5 @@ builder.Services.AddMassTransit(x =>
 var app = builder.Build();
 
 app.UseHttpsRedirection();
-
-app.MapGet("/send-email", async () =>
-{
-        //Нагрузочное тестирование
-        // for (int i = 0; i < 1000; i++) // Задайте количество писем
-        // {
-        //     var mail = new MailMessage
-        //     {
-        //         From = new MailAddress("your_email@example.com"),
-        //         Subject = $"Test Email {i + 1}",
-        //         Body = "This is a test email.",
-        //         IsBodyHtml = false
-        //     };
-        //     mail.To.Add("recipient@example.com");
-
-        //     try
-        //     {
-        //         await client.SendMailAsync(mail);
-        //         Console.WriteLine($"Email {i + 1} sent successfully.");
-        //     }
-        //     catch (Exception ex)
-        //     {
-        //         Console.WriteLine($"Failed to send email {i + 1}: {ex.Message}");
-        //     }
-        // }
-        
-})
-.WithName("send-email");
 
 app.Run();
