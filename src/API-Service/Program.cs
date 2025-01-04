2b0abe160da1382b88f1b619237187e9c19c6739
Daniil <danilmux@gmail.com>
2025-01-04 07:18:43 +0500

    add settings configuration for RabbitMq

--- src/API-Service/Program.cs
+++ src/API-Service/Program.cs
@@ -37,6 +37,11 @@ namespace API_Service
 
                 x.UsingRabbitMq((context, cfg) =>
                 {
+                    cfg.Host(builder.Configuration["RabbitMq:Host"], "/", host => {
+                        host.Username(builder.Configuration.GetValue("RabbitMq:Username", "guest"));
+                        host.Password(builder.Configuration.GetValue("RabbitMq:Password", "guest"));
+                    });
+
                     cfg.ReceiveEndpoint("email-notification-sent", e => {
                         e.UseMessageRetry(r => r.Interval(5, 5));
 
--- src/Email-Service/Program.cs
+++ src/Email-Service/Program.cs
@@ -32,7 +32,13 @@ builder.Services.AddMassTransit(x =>
 
     x.UsingRabbitMq((context, cfg) =>
     {
-        cfg.ReceiveEndpoint("email-notification-created", e => {
+        cfg.Host(builder.Configuration["RabbitMq:Host"], "/", host => {
+            host.Username(builder.Configuration.GetValue("RabbitMq:Username", "guest"));
+            host.Password(builder.Configuration.GetValue("RabbitMq:Password", "guest"));
+        });
+
+        cfg.ReceiveEndpoint("email-notification-created", e => 
+        {
             e.UseMessageRetry(r => r.Interval(5, 5));
 
             e.ConfigureConsumers(context);
